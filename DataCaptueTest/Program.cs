ad3d5ab614cd4282e6f858dbd1dfa1bce0ee8d8d
sticwin <ycf460@gmail.com>
2020-03-29 10:34:50 +0800

    3-29欧哲测试
    3月29号欧哲测试

--- DataCaptueTest/Program.cs
+++ DataCaptueTest/Program.cs
@@ -15,7 +15,7 @@ namespace DataCaptueTest
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new btnTest());
+            Application.Run(new MainForm());
         }
     }
 }
